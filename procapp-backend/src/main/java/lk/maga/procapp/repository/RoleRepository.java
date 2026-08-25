62697302cf8d4dc1b46fb1c508d0a5f0c868fd04
MAGA-DEV03 <dev03.it@maga.lk>
2026-08-25 11:54:21 +0530

    Add Role CRUD feature

---
+++ procapp-backend/src/main/java/lk/maga/procapp/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package lk.maga.procapp.repository;
+
+import lk.maga.procapp.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    boolean existsByNameIgnoreCase(String name);
+    
+}
